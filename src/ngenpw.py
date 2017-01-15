acd7bbbaedc035e6a1671386b4e76d722b44de3f
Masashi Teruya <MasashiTeruya@outlook.com>
2017-01-16 01:42:03 +0900

    Add ignore charactors option

--- src/ngenpw.py
+++ src/ngenpw.py
@@ -9,13 +9,36 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-n', '--number', metavar='N', type=int,
                     help='password length',
                     default=DEFAULT_LENGTH)
+parser.add_argument('-ia', '--ignore-ascii-charactors',
+                    action='store_true',
+                    help='excludes ascii charactors from being used',
+                    default=False)
+parser.add_argument('-in', '--ignore-numbers',
+                    action='store_true',
+                    help='excludes numbers from being used',
+                    default=False)
+parser.add_argument('-is', '--ignore-special-charactors',
+                    action='store_true',
+                    help='excludes special charactors from being used',
+                    default=False)
 args = parser.parse_args()
-length = args.number
 
-ascii = string.ascii_letters
-digits = string.digits
-special_charactors = '"?!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~"'
-charactors = ascii + digits + special_charactors
+length = args.number
+ignore_ascii_charactors = args.ignore_ascii_charactors
+ignore_numbers = args.ignore_numbers
+ignore_special_charactors = args.ignore_special_charactors
+
+charactors = ''
+if not ignore_ascii_charactors:
+    charactors += string.ascii_letters
+
+if not ignore_numbers:
+    charactors += string.digits
+
+if not ignore_special_charactors:
+    special_charactors = '"?!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~"'
+    charactors += special_charactors
+
 password = ''.join(random.sample(charactors, length))
 
 print(password)
